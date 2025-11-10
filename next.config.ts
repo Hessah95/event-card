40c11cdd9d2e51c83ca27072d62e5c78fa561e98
Hessah Alhamadi <hessah.alhamadi.95@gmail.com>
2025-11-10 15:36:35 +0300

    changes on the card

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.glb$/,
+      type: "asset/resource", // Treat as a static asset
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
